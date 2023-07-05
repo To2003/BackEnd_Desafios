703ff5706e86743a91dbee423e925a6f9934821b
To2003 <114697784+To2003@users.noreply.github.com>
2023-07-05 10:38:36 -0300

    Update ECMAScript.js

--- ECMAScript.js
+++ ECMAScript.js
@@ -1,79 +1,35 @@
-const manager = new ProductManager();
-
-const product1 = {
-    title: "Producto 1",
-    description: "Iphone Iphone Iphone Iphone Iphone",
-    price: 10000,
-    thumbnail: "https://www.apple.com/la/newsroom/2020/10/apple-introduces-iphone-12-pro-and-iphone-12-pro-max-with-5g/",
-    code: "123ZZZ",
-    stock: 25
-}
-
-const product2 = {
-    title: "Producto 1",
-    description: "Iphone Iphone Iphone Iphone Iphone",
-    price: 10000,
-    thumbnail: "https://www.apple.com/la/newsroom/2020/10/apple-introduces-iphone-12-pro-and-iphone-12-pro-max-with-5g/",
-    code: "AAA123",
-    stock: 25
-}
-
-class ProductManager {
-    constructor() {
-        this.products = [];
-        this.id= 0;
-    }
-
-    checkProducto(product){
-        return (
-            product.title && 
-            product.description && 
-            product.price && 
-            product.thumbnail && 
-            product.code && 
-            product.stock
-            );
-    }
-
-    isCodeDuplicated(code) {
-        return this.products.some(product => product.code === code);
-    }
+class ProductManager{
+    constructor(){
+        this.products=[]
     
-    addProduct(product) {
-        if(!this.checkProducto(product)) {
-            console.log("Error: Completar todos los datos.");
-            return;
-        }
-        if(this.isCodeDuplicated(product.code)) {
-            console.log("Error: Este codigo de producto ya existe.");
-            return;
-        }
-        const newProduct = {
-            ...product,
-            id: ++this.id
-        }
-        this.products.push(newProduct);
+    }
+    static id=0
+    addProduct(title, description, price, thumbmail, code, stock){
+        ProductManager.id++
+        this.products.push({title, description, price, thumbmail, code, stock, id:ProductManager.id});
     }
 
-    getProducts() {
-        console.log(this.products);
+    getProduct(){
+        return this.products
     }
 
-    getProductById(id) {
-        const product = this.products.find(product => product.id === id);
-        if(!product) {
-            console.log("Error 404! Not Found");
-            return; 
-        }
-        else{
-            console.log(product);
+    getProductById(id){
+        if(!this.products.find((product)=> product.id === id)){
+            return 'Error 404! Not Found'
+        }else{
+            return this.products.find((product)=> product.id === id)
         }
     }
 }
+ const productos = new ProductManager();
 
-manager.getProducts();
-manager.addProduct(product1);
-manager.getProducts();
-manager.addProduct(product2);
-manager.getProducts();
-manager.getProductById(1);
+ console.log(productos.getProduct());
+
+ productos.addProduct('Iphone 12','12 Pro Max', 10000,'https://www.apple.com/la/newsroom/2020/10/apple-introduces-iphone-12-pro-and-iphone-12-pro-max-with-5g/','123ABC', 5)
+ productos.addProduct('Iphone 13','13 Pro Max', 15000,'https://www.apple.com/la/newsroom/2020/10/apple-introduces-iphone-12-pro-and-iphone-12-pro-max-with-5g/','987CBA', 12)
+
+ console.log(productos.getProduct());
+
+ console.log(productos.getProductById(2));
+
+ console.log(productos.getProductById(3));
