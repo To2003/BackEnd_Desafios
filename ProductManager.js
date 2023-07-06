e6b7c523b893eb692cb0ccff2b3bc2c49b66a449
To2003 <114697784+To2003@users.noreply.github.com>
2023-07-06 10:57:34 -0300

    Update ProductManager.js

--- ProductManager.js
+++ ProductManager.js
@@ -2,59 +2,60 @@ const fs = require('fs');
 
 class ProductManager {
   constructor(filePath) {
-    this.path = filePath;
-    this.products = [];
-    this.loadProducts();
+    this.path = filePath; // Ruta del archivo donde se guardarán los productos
+    this.products = []; // Arreglo donde se almacenarán los productos en memoria
+    this.loadProducts(); // Cargamos los productos desde el archivo
   }
 
   loadProducts() {
-    try {
-      const fileData = fs.readFileSync(this.path, 'utf-8');
-      this.products = JSON.parse(fileData);
-    } catch (error) {
-      // Si ocurre un error al leer el archivo o el archivo no existe,
-      // se mantendrá el arreglo de productos vacío.
-      this.products = [];
+  try {
+    const fileData = fs.readFileSync(this.path, 'utf-8'); // Leemos el archivo de forma síncrona
+    this.products = JSON.parse(fileData); // Convertimos el contenido del archivo a un objeto JavaScript
+    } 
+  catch (error) {
+    // Si ocurre un error al leer el archivo o el archivo no existe,
+    // se mantendrá el arreglo de productos vacío.
+    this.products = [];
     }
   }
 
   saveProducts() {
-    const data = JSON.stringify(this.products, null, 2);
-    fs.writeFileSync(this.path, data, 'utf-8');
-  }
+  const data = JSON.stringify(this.products, null, 2); // Convertimos el arreglo de productos a formato JSON
+  fs.writeFileSync(this.path, data, 'utf-8'); // Escribimos el contenido en el archivo de forma síncrona
+}
 
   addProduct(product) {
-    ProductManager.id++;
-    const newProduct = {
-      id: ProductManager.id,
-      ...product
-    };
-    this.products.push(newProduct);
-    this.saveProducts();
+  ProductManager.id++; // Incrementamos el ID estático de la clase (con cada nuevo producto agregado)
+  const newProduct = {
+    id: ProductManager.id, // Asignamos el ID al nuevo producto
+    ...product // Agregamos las demás propiedades del producto
+  };
+  this.products.push(newProduct); // Agregamos el nuevo producto al arreglo de productos
+  this.saveProducts(); // Guardamos los productos en el archivo
+}
+
+ getProduct() {
+  return this.products; // Devolvemos el arreglo de productos
+}
+
+getProductById(id) {
+  const product = this.products.find((product) => product.id === id); // Buscamos el producto por su ID
+  if (product) {
+    return product; // Si se encuentra el producto, lo devolvemos
+  } else {
+    return 'Error 404! Not Found'; // Si no se encuentra, devolvemos un mensaje de error
+  }
   }
 
-  getProduct() {
-    return this.products;
-  }
-
-  getProductById(id) {
-    const product = this.products.find((product) => product.id === id);
-    if (product) {
-      return product;
-    } else {
-      return 'Error 404! Not Found';
-    }
-  }
-
-  updateProduct(id, updatedFields) {
-    const productIndex = this.products.findIndex((product) => product.id === id);
-    if (productIndex !== -1) {
-      this.products[productIndex] = {
-        ...this.products[productIndex],
-        ...updatedFields
-      };
-      this.saveProducts();
-      return true;
+ updateProduct(id, updatedFields) {
+  const productIndex = this.products.findIndex((product) => product.id === id); // Buscamos el índice del producto a actualizar
+  if (productIndex !== -1) {
+    this.products[productIndex] = {
+      ...this.products[productIndex],
+      ...updatedFields
+    }; // Actualizamos los campos del producto con los valores proporcionados en `updatedFields`
+    this.saveProducts(); // Guardamos los productos actualizados en el archivo
+    return true
     } else {
       return false;
     }
@@ -76,4 +77,4 @@ class ProductManager {
 const filePath = 'products.json';
 const productos = new ProductManager(filePath);
 
-console.log(productos.getProduct
+console.log(productos.getProduct)
