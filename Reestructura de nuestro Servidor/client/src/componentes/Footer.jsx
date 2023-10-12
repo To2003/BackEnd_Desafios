f168401cd4e8a33e08dca430c5a08fc12dab685c
To2003 <soytomasaguilar@gmail.com>
2023-10-11 22:13:10 -0300

    Desafio 11 completo

---
+++ Reestructura de nuestro Servidor/client/src/componentes/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+    return <footer className="footer">
+        <p>Aguilar Tomas 2023 - Portafolio</p>
+    </footer>
+}
+
+export default Footer;
