4ff3f87d9abc59a68fef4fa88dc061aa234b5ec9
To2003 <soytomasaguilar@gmail.com>
2023-11-07 21:50:48 -0300

    Mocking y Manejo de Errores 2.0

---
+++ Mocking y Manejo de Errores/client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+    return <footer className="footer">
+        <p>Tomas Aguilar 2023 - Portafolio COMING SOON</p>
+    </footer>
+}
+
+export default Footer;
